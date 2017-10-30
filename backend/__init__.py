19a636b4980b0ea3e3b176470aa128cf21429bdf
Tessavardy <32839656+Tessavardy@users.noreply.github.com>
2017-10-30 14:20:48 +0100

    PrintAlex

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,3 +1,11 @@
 def process_user_query(query_string): ## query_string stores what the user enters on heruki
-    result = "Good Morning {query_string}"
-    return result
+
+    list1 = query_string.split()
+
+    for n in list1:
+        result = f'Hi {query_string}'
+
+    return result ## return should always be something like this
+
+
+print(process_user_query('Tessa Alex'))
