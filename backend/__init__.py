0f3fd0f71288c5bd0f38b4af0507d1abca678e75
Tessavardy <32839656+Tessavardy@users.noreply.github.com>
2017-10-30 13:53:11 +0100

    ChangedAgain

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,3 +1,3 @@
-def process_user_query(query_string):
-    result = {"hi There"}
+def process_user_query(query_string): ## query_string stores what the user enters on heruki
+    result = "hi There"
     return result
