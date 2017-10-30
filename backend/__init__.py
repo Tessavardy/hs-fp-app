e7a2c3f1ad4a96e5cd134bcea03b9f1400bb846e
Tessavardy <32839656+Tessavardy@users.noreply.github.com>
2017-10-30 19:54:52 +0100

    ss

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,17 +1,8 @@
-# def process_user_query(query_string): ## query_string stores what the user enters on heruki
-#
-#     list1 = query_string.split() ##creating it into a list of names
-#     names = []
-#     for name in list1:
-#         names.append(f'Hi {name}!')
-#     return names ## return should always be something like this
-#
-#
-# print(process_user_query('Tessa Alex'))
 
 def process_user_query(query_string): ## query_string stores what the user enters on heruki
 
-list1 = ['Hi alex how are you']
+    list1 = query_string.split() ##creating it into a list of names
+    names = []
 for word in list1:
     if word[0].isupper():
         list1.append(word)
