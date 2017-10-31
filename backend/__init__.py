c7caa51fffa0eab7db6f3fa7f688f3c9bc8d6476
Tessavardy <32839656+Tessavardy@users.noreply.github.com>
2017-10-31 15:40:18 +0100

    Lastone

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,9 @@
 
-def process_user_query(query_string): ## query_string stores what the user enters on heruki
-
+def process_user_query(query_string): ## query_string stores what the user enters on heruku
     list1 = query_string.split() ##creating it into a list of names
     names = []
-for word in list1:
-    if word[0].isupper():
-        list1.append(word)
-
-print(list1)
+    for word in set(list1):
+        if word[0].isupper():
+            names.append(f' Hi {word}')
+    return(names)
+ 
